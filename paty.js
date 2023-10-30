8d33a1eab6661923f02f974cbd2f0914103e835a
Oleksandra <sunnyem@seznam.cz>
2023-10-30 18:53:57 +0100

    commit

---
+++ paty.js
@@ -0,0 +1,7 @@
+function calculateTotalPrice(order) {
+    for (let i = 0; i < order.length; i++) {
+      return order.push(i);
+    }
+  }
+
+  console.log(calculateTotalPrice([412, 371, 94, 63, 176]));
