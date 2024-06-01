71a45e356bd35913dd0f7bdda634a6e604fd1874
Oleksandra <sunnyem@seznam.cz>
2024-06-01 02:44:56 +0200

    commit

--- dvactctverti.js
+++ dvactctverti.js
@@ -5,8 +5,12 @@ const players = [
     { name: "Kiwi", playtime: 241, gamesPlayed: 1 },
   ];
   
-  const totalAveragePlaytimePerGame = players.reduce((acc,player) => 
-    { return players.playtime / players.gamesPlayed ;
-  } ,0);
+  // const totalAveragePlaytimePerGame = players.reduce((acc,player) => { acc + player;
+  //   return players.playtime / players.gamesPlayed ;
+  // },0);
+
+  // const totalAveragePlaytimePerGame = (players) => players.reduce((acc , player) => {return player.playtime / player.gamesPlayed},0);
+  const totalAveragePlaytimePerGame = players.reduce(((acc,player) => player.playtime / player.gamesPlayed + acc),0);
   
-  console.log(totalAveragePlaytimePerGame)
+  
+  console.log(totalAveragePlaytimePerGame);
