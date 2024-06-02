9250e604b02720b33fc2441a06398508e6fb44c1
Oleksandra <sunnyem@seznam.cz>
2024-06-02 22:13:17 +0200

    commit

---
+++ dvadcatsedmi.js
@@ -0,0 +1,17 @@
+const pizzaPalace = {
+    pizzas: ["Supercheese", "Smoked", "Four meats"],
+    checkPizza(pizzaName) {
+      return pizzas.includes(pizzaName);
+    },
+    order(pizzaName) {
+      const isPizzaAvailable = this.pizzaName;
+  
+      if (!isPizzaAvailable) {
+        return `Sorry, there is no pizza named «${pizzaName}»`;
+      }
+  
+      return `Order accepted, preparing «${pizzaName}» pizza`;
+    },
+  };
+  console.log(pizzaPalace.order("Big Mike"));
+  console.log(pizzaPalace.order("Four meats"));
