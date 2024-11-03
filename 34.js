69461d367841e2e3cbe78a33fd415d015c08cefd
Oleksandra <sunnyem@seznam.cz>
2024-11-03 18:13:23 +0100

    commit

--- 34.js
+++ 34.js
@@ -2,3 +2,8 @@ const LS_KS = 'Array of names';
 const names = ['Alice' , 'Kate', 'Anna'];
 
 localStorage.setItem(LS_KS , JSON.stringify(names));
+
+const currentDate = new Date();
+console.log(currentDate);
+const newDate = new Date('9/5/2024');
+console.log(newDate);
