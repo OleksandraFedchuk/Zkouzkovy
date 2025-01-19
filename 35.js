6c294abd90485d7618926e12aa06ed9a084063a5
Oleksandra <sunnyem@seznam.cz>
2025-01-20 00:12:28 +0100

    commit

--- 35.js
+++ 35.js
@@ -1,13 +1,18 @@
 const todolist = document.querySelector('.todolist');
 console.log(todolist);
 
+const createMarkUp = ({completed , title}) =>
+    `<li class=${completed ? "done" : "in-progress"}>${title}</li>`;
 
+const createLiArr = (liArr) => liArr.map(createMarkUp).join("");
 
-fetch("https://jsonplaceholder.typicode.com/todos1").then((res) => {console.log(res);
+fetch("https://jsonplaceholder.typicode.com/todos").then((res) => {console.log(res);
     if(!res.ok){
         throw new Error(res.status)
     }
     return res.json(); 
 }).then((data) => {console.log(data);
+    const listMarkap = createLiArr(data);
+    todolist.insertAdjacentHTML("beforeend", listMarkap)
 }).catch((error) => {console.error("Fetch Error:",error);
 })
